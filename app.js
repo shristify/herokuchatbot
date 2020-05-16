1e155e33af251205b41ec46d610a08b8beb9ab12
Shristi Singh <55556359+confusedcoder1@users.noreply.github.com>
2020-05-16 21:24:01 +0530

    Add files via upload
    Node modules and credentials.json not uploaded.

---
+++ app.js
@@ -0,0 +1,79 @@
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+const express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
+var port = process.env.PORT ||8080
+app.use(express.static(__dirname + "/public"));
+app.get("/", function(req,res){
+    res.render("index");
+})
+
+const sessionId = uuid.v4();
+app.use(bodyParser.urlencoded({
+    extended:false
+}))
+
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
+app.post('https://fast-atoll-84457.herokuapp.com/',(req,res)=>{
+
+   runSample(req.body.MSG).then(data=>{
+       res.send({Reply:data})
+   })
+})
+/** 
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(msg,projectId = 'nairobi-mtlbnp') {
+  // A unique identifier for the given session
+  
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+      keyFilename:"hosting/Nairobi-edcfd08eadbd.json"
+  });
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: msg,
+        // The language used by the client (en-US)
+        languageCode: 'en-US',
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }
+  return result.fulfillmentText;
+}
+
+app.listen(port, function(){
+  console.log("app.running" +port);
+})
+
